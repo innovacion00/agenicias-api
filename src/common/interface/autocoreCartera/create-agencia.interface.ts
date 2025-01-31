8133a7711ef97dcbc40476f708e8f5dcffc187ec
Innovacion <innovacion@gehsuites.com>
2025-01-31 12:31:22 -0500

    nueva interface de crear agencia body

--- src/common/interface/autocoreCartera/create-agencia.interface.ts
+++ src/common/interface/autocoreCartera/create-agencia.interface.ts
@@ -1,20 +1,20 @@
 interface Agency {
-    name:                    string;
-    phone:                   string;
-    rc_identifier:           null;
-    country_code:            string;
-    oneopay_user_id:         null;
-    is_active:               boolean;
-    nuvei_user_id:           null;
-    email_for_notifications: string;
-    document_type:           string;
-    created_at:              Date;
-    document_number:         string;
-    id:                      number;
-    hyperguest_token:        null;
-    updated_at:              Date;
-    is_archived:             boolean;
-    cobre_account_id:        string;
+  name: string;
+  phone: string;
+  rc_identifier: null;
+  country_code: string;
+  oneopay_user_id: null;
+  is_active: boolean;
+  nuvei_user_id: null;
+  email_for_notifications: string;
+  document_type: string;
+  created_at: Date;
+  document_number: string;
+  id: number;
+  hyperguest_token: null;
+  updated_at: Date;
+  is_archived: boolean;
+  cobre_account_id: string;
 }
 
 export interface ICreateAgenciaBody {
@@ -24,11 +24,12 @@ export interface ICreateAgenciaBody {
   country_code: string;
   document_type: string;
   document_number: string;
+  is_preloaded: boolean;
+  cobre_account_id: string;
 }
 
 export interface ICreateAgenciaResponce {
-    msg:    string;
-    id:     number;
-    agency: Agency;
+  msg: string;
+  id: number;
+  agency: Agency;
 }
-
