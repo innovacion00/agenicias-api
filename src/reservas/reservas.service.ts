21ac92b08fc8cee16cdde1c9dd5fcfda7a9cdc45
Innovacion <innovacion@gehsuites.com>
2025-02-13 12:56:32 -0500

    cancelar reserva

--- src/reservas/reservas.service.ts
+++ src/reservas/reservas.service.ts
@@ -311,14 +311,18 @@ export class ReservasService {
 
       if (
         !user.role.includes('admin') &&
-        !user.reservas.includes(cancelReservaDto.reservaId)
+        !user.reservas.includes(cancelReservaDto.reservaId) &&
+        !user.role.includes('super-admin')
       ) {
         throw new ForbiddenException(
           'No cuentas con los permisos necesarios para cancelar esta reserva',
         );
       }
 
-      if (reserva.agenciaId.toString() !== user.agencia.toString()) {
+      if (
+        reserva.agenciaId.toString() !== user.agencia.toString() &&
+        !user.role.includes('super-admin')
+      ) {
         throw new ForbiddenException(
           'No cuentas con los permisos necesarios para cancelar esta reserva',
         );
@@ -485,12 +489,17 @@ export class ReservasService {
   }
 
   // ? Pruebas
-  // async prueba(borrarDto: BorrarDto) {
+  // async prueba() {
   //   try {
-  //     const user = await this.userModel.findById(borrarDto.userId);
-  //     const reserva = await this.reservasModel.findById(borrarDto.reservaId);
+  //     const agencia = await this.userModel
+  //       .findById('677d7753155954115cea20aa')
+  //       .populate('agencia', 'empresa');
 
-  //     return user.reservas.includes(borrarDto.reservaId);
+  //     const user = agencia.toJSON();
+
+  //     return {
+  //       terminado: user,
+  //     };
   //   } catch (error) {
   //     this.logger.error(error);
   //     this.errorManager.handle(error);
