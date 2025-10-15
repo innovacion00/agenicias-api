382bfa32f7570cfe907284959e2f8a01b1ade26a
carlosceballos1 <carlosdceballos30@gmail.com>
2025-10-15 12:56:33 -0500

    fechalimiterespuesta opcional

--- src/cotizaciones/dto/create-cotizacion.dto.ts
+++ src/cotizaciones/dto/create-cotizacion.dto.ts
@@ -405,7 +405,7 @@ export class CreateCotizacionDto {
   @IsNotEmpty()
 
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   @Matches(/^\d{4}-\d{2}-\d{2}$/, {
     message: 'fechaLimiteRespuesta debe venir en formato YYYY-MM-DD',
   })
